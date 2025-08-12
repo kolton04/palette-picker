b1533a66dfb778dd9efa3e93b653ed929344ea61
Kolton04 <kolton.tolliver@gmail.com>
2025-08-11 23:41:59 -0400

    split complementary still bugged. fixed analogous to prevent duplicate hues.

--- patterns.js
+++ patterns.js
@@ -43,21 +43,36 @@ function analogous() {
         case 12:
             const randomIndex = Math.floor(Math.random() * swatches.length);
             const randomSwatch = swatches[randomIndex];
-            let randomHue;
 
-            minHue = swatches[0].hue;
-            maxHue = swatches[2].hue;
+            let hues = new Set();
+            for(let i = 0; i < swatches.length; i++){
+                hues.add(swatches[i].hue);
+            }
 
-            //wraps numbers around 360 if minHue is close to 360
-            if(minHue <= maxHue){
-                randomHue = Math.random() * (maxHue - minHue) + minHue;
+            minHue = Math.min(...hues);
+            maxHue = Math.max(...hues);
+            
+            console.log(hues.size);
+            if(hues.size <= 2){
+                updateHue = (randomSwatch.hue - 35);
+                if(hues.has(updateHue)){
+                    updateHue = (randomSwatch.hue + 35);
+                }
             }
             else{
-                let randomHueStep = Math.floor(Math.random() * 70)
-                randomHue = (minHue + randomHueStep) % 360;
-            }
+                //wraps numbers around 360 if minHue is close to 360
+                if(minHue <= maxHue){
+                    updateHue = Math.round((Math.random() * (maxHue - minHue) + minHue) % 360);
+                }
+                else{
+                    updateHue = (minHue + (Math.floor(Math.random() * 70))) % 360;
+                }
 
-            swatches.push(new Swatch(randomHue, 
+            }
+            console.log(swatches);
+            console.log(updateHue)
+
+            swatches.push(new Swatch(updateHue, 
                 Math.random() * ((randomSwatch.sat + 0.1) - (randomSwatch.sat - 0.1)) + (randomSwatch.sat - 0.1),
                 Math.random() * ((randomSwatch.light + 0.1) - (randomSwatch.light - 0.1)) + (randomSwatch.light - 0.1)
             ))
@@ -123,10 +138,12 @@ function splitComp() {
     // 1 [initial hue]
     // 2 [intial hue + (180 + offset)] 
     // 3 [intial hue + (180 - offset)]
-    const hueOffset = 20;
+    const hueOffset = 30;
+    let hues = [];
 
     switch(swatches.length){
         case 0:
+
             let h = Math.floor(Math.random() * 360);
             for(let i = 0; i < 3; i++){                
                 if(i < 2){
@@ -136,52 +153,34 @@ function splitComp() {
                     updateHue = (h + (180 - hueOffset)) % 360;
                 }
                 swatches.push(new Swatch(updateHue, s, l));
+                console.log(swatches[i].hue)
+
             }
             break;
-        // 50/50 to make next hue [180 + offset] OR [180 - offset].
+        // 33/33/33 to make next hue +200 OR +180 OR hue +0.
         case 1:
-
             if(Math.random() < 0.5){
-                updateHue = ((swatches[0].hue + (180 + hueOffset)) % 360);
+                updateHue = ((swatches[0].hue + 180) + hueOffset) % 360;
+                added = true;
             }
             else{
-                updateHue = ((swatches[0].hue + (180 - hueOffset)) % 360);
+                updateHue = ((swatches[0].hue + 180) - hueOffset) % 360;
             }
             swatches.push(new Swatch(updateHue, s, l));
-            console.log(swatches)
             break;
-        // 
         case 2:
-            const firstHue = swatches[0].hue;
-            const secondHue = swatches[1].hue;
-            let hueDiff;
-
-            if(firstHue > secondHue){
-                hueDiff = firstHue - secondHue;
-            }
-            else if (secondHue > firstHue){
-                hueDiff = secondHue - firstHue;
-            }
-            console.log(hueDiff);
-
-            if(hueDiff === 160){
-                if(Math.random() < 0.5){
-                    updateHue = ((swatches[1].hue + (180 + hueOffset)) % 360);
-                }
-                else{
-                    updateHue = ((swatches[1].hue + (180 - hueOffset)) % 360);
-                }
-            }
-            else if (hueDiff === 200){
-                if(Math.random() < 0.5){
-                    updateHue = ((swatches[1].hue + (180 - hueOffset)) % 360);
-                }
-                else{
-                    updateHue = ((swatches[1].hue + (180 + hueOffset)) % 360);
-                }
+            hues = [];
+            for(let i = 0; i < swatches.length; i++){
+                hues.push(swatches[i].hue);
             }
 
+            updateHue = ((swatches[0].hue + 180) - hueOffset) % 360;
+            const duplicateHue = hues.includes(updateHue);
 
+            if(duplicateHue){
+                updateHue = ((swatches[0].hue + 180) + hueOffset) % 360;
+            }
+        
             swatches.push(new Swatch(updateHue, s, l));
             console.log(swatches)
             break;
@@ -195,26 +194,26 @@ function splitComp() {
         case 10:
         case 11:
         case 12:
-            const randomIndex = Math.floor(Math.random() * swatches.length);
+            const randomIndex = Math.floor(Math.random() * 3);
             const randomSwatch = swatches[randomIndex];
-            let randomHue;
-
-            minHue = swatches[0].hue;
-            maxHue = swatches[2].hue;
-
-            //wraps numbers around 360 if minHue is close to 360
-            if(minHue <= maxHue){
-                randomHue = Math.random() * (maxHue - minHue) + minHue;
-            }
-            else{
-                let randomHueStep = Math.floor(Math.random() * 70)
-                randomHue = (minHue + randomHueStep) % 360;
+            hues = [];
+            for(let i = 0; i < 3; i++){
+                hues.push(swatches[i].hue);
             }
 
-            swatches.push(new Swatch(randomHue, 
+            console.log(hues);
+
+            let checkUnique = new Set(hues);
+
+            if(checkUnique.size === 3){
+                swatches.push(new Swatch(randomSwatch.hue, 
                 Math.random() * ((randomSwatch.sat + 0.1) - (randomSwatch.sat - 0.1)) + (randomSwatch.sat - 0.1),
                 Math.random() * ((randomSwatch.light + 0.1) - (randomSwatch.light - 0.1)) + (randomSwatch.light - 0.1)
             ))
+            }
+            else {
+
+            }
             break;    
     }
 }
